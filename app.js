181a02eb8aa2100bae4ed77fccf580d2c44bf6dc
Christian <christianbur1248@gmail.com>
2024-01-10 04:28:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors({
   credentials: true,
-  origin: '*'
+  origin: 'https://chrisdav03.github.io/'
 }))
 
 // Route will be localhost:4000

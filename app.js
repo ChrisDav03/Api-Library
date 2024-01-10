1b97e05e1abab7f2c543fc0c436ff8b0d948e90e
Christian <christianbur1248@gmail.com>
2024-01-10 04:38:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors({
   credentials: true,
-  origin: 'https://chrisdav03.github.io/vite-deploy/'
+  origin: '*'
 }))
 
 // Route will be localhost:4000
